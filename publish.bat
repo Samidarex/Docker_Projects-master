426fbee496dc306cf3de7a780b1f2c7eadc72f1c
Marcel Prodan <nuta228322@gmail.com>
2022-01-16 01:07:18 +0200

    fix

--- publish.bat
+++ publish.bat
@@ -1,10 +1,6 @@
-
 @echo off
 
-set "docker_image_exists = false"
+set "docker_image_exists=false"
 
-if %docker_image_exists% == "false" (
-  docker pull build.target.com\my-app:latest
-) ELSE (
-  echo "Docker image already exists, skipping pull"
-)
+if %docker_image_exists% == "false" ( docker pull build.target.com\my-app:latest ) 
+ELSE ( echo "Docker image already exists, skipping pull" )
