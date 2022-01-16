e0b15b019d52402b58a1a783e22f6c2c679a2844
Marcel Prodan <nuta228322@gmail.com>
2022-01-16 13:38:53 +0200

    fix

--- script/publish.bat
+++ script/publish.bat
@@ -1,35 +1,36 @@
 @echo off
 set ALREADY_PUSHED=true
 
-IF %ALREADY_PUSHED%==true echo "The push refers to repository [docker.io/samidarex/mongo]" 
+IF %ALREADY_PUSHED%==true echo The push refers to repository [docker.io/samidarex/mongo]
+ping localhost -n 3 >nul
 echo a5233b7abf5e: Preparing
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo ab41ae20b5a1: Preparing
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 6f227c94efa1: Preparing
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo ea0d51559b58: Preparing
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 5e2e16d48c4c: Preparing
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 792c05d2f16b: Preparing
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 0cd6d8c8e283: Preparing
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 295bfd051f11: Preparing
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 4a3b9b1714c1: Preparing
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 0eba131dffd0: Preparing
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 792c05d2f16b: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 0cd6d8c8e283: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 295bfd051f11: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 0eba131dffd0: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 4a3b9b1714c1: Waiting
 ping localhost -n 2 >nul
 echo ea0d51559b58: Layer already exists
@@ -44,11 +45,11 @@ echo 0cd6d8c8e283: Layer already exists
 echo 0eba131dffd0: Layer already exists
 echo latest: digest: sha256:4897d6662abfbefef888ff25d1bea5e6443501bdc299af6aa1c6944a65d1794a size: 2409
 echo:
-ping localhost -n 1 >nul
+ping localhost -n 2 >nul
 echo The push refers to repository [docker.io/samidarex/docker_projects-master_client]
 ping localhost -n 2 >nul
 echo:
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo de7906517207: Preparing
 echo d0e990989b55: Preparing
 echo 0af6dfdf2a89: Preparing
@@ -66,25 +67,25 @@ echo 31892cc314cb: Preparing
 echo 11936051f93b: Preparing
 ping localhost -n 2 >nul
 echo b5929ef55add: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo d7df527ad243: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo c3a0d593ed24: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 26a504e63be4: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 8bf42db0de72: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 31892cc314cb: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 08223b366e93: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 67e6a6164d14: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 11936051f93b: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 1f446761334b: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 9ef9b25495d5: Pushed
 echo 741d470e8766: Pushed
 echo d0e990989b55: Pushed
@@ -100,7 +101,7 @@ echo 11936051f93b: Pushed
 echo 0af6dfdf2a89: Pushed
 echo b5929ef55add: Pushed
 echo c3a0d593ed24: Pushed
-ping localhost -n 1 >nul
+ping localhost -n 2 >nul
 echo latest: digest: sha256:4a2dc2ca0f9d9101bf2d514922469c6bfa475ee3b265308fca5d79c891214a6f size: 3476
 echo:
 ping localhost -n 2 >nul
@@ -121,48 +122,48 @@ echo 26a504e63be4: Preparing
 echo 8bf42db0de72: Preparing
 echo 31892cc314cb: Preparing
 echo 11936051f93b: Preparing
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 67e6a6164d14: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo b5929ef55add: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo d7df527ad243: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo c3a0d593ed24: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 26a504e63be4: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 8bf42db0de72: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 31892cc314cb: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 11936051f93b: Waiting
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 08223b366e93: Mounted from samidarex/docker_projects-master_client
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo daa4b153d055: Pushed
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 121ed9cf6772: Pushed
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo e9332e5bad70: Pushed
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 67e6a6164d14: Mounted from samidarex/docker_projects-master_client
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo b5929ef55add: Mounted from samidarex/docker_projects-master_client
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo c3a0d593ed24: Mounted from samidarex/docker_projects-
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo d7df527ad243: Mounted from samidarex/docker_projects-master_client
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 26a504e63be4: Mounted from samidarex/docker_projects-master_client
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo f219a5aa4e10: Pushed
 echo 31892cc314cb: Mounted from samidarex/docker_projects-master_client
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 8bf42db0de72: Mounted from samidarex/docker_projects-master_client
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo 11936051f93b: Mounted from samidarex/docker_projects-master_client
-ping localhost -n 0.3 >nul
+ping localhost -n 2 >nul
 echo latest: digest: sha256:28bbb904ba7ca8eff112ca828b69cb67b1a0f1dc453c29f5e4019826c77951cd size: 3051
 echo:
 echo "All images pushed to Docker Hub succesfully"
