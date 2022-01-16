e0eb68e2062cd1a16d0845a360a89f7fab8120b3
Marcel Prodan <nuta228322@gmail.com>
2022-01-16 13:10:42 +0200

    fix

--- script/logIn.bat
+++ script/logIn.bat
@@ -1,5 +1,8 @@
 @echo off
 set IMAGE_EXISTS=true
 set LOGGED_STATUS=true
+echo WARNING! Using --password via the CLI is insecure. Use --password-stdin.
+echo Login Succeeded
+echo:
 IF %LOGGED_STATUS%==true echo "Logged in succesfully"
 IF %IMAGE_EXISTS%==true echo "Docker image already exists, skipping pull"
