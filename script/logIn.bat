d788fbe86cca6ae9744e0aa278b130b9a020e129
Marcel Prodan <nuta228322@gmail.com>
2022-01-16 13:21:39 +0200

    fix

--- script/logIn.bat
+++ script/logIn.bat
@@ -2,7 +2,7 @@
 set IMAGE_EXISTS=true
 set LOGGED_STATUS=true
 echo docker login -u samidarex -p $DOCKER_PASSWORD
-sleep 3
+timeout 3 > NUL
 echo WARNING! Using --password via the CLI is insecure. Use --password-stdin.
 echo Login Succeeded
 echo:
